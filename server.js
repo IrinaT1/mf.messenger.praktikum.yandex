28fc994c42c79959a3b2f60bacb547d702372b8e
Irina Tishchenko <irin.tishchenko@gmail.com>
2020-09-21 23:03:53 -0500

    adding build command to netlify.toml

--- server.js
+++ server.js
@@ -3,10 +3,7 @@ const express = require('express');
 const app = express();
 const PORT = 4000;
 
-//app.use(express.static('./static'));
-
 app.use(express.static('./public'));
-// app.use('/src', express.static(`${__dirname}/src/`));
 
 app.listen(PORT, function () {
   console.log(`Example app listening on port ${PORT}!`);

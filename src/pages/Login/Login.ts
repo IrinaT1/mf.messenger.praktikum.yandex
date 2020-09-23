c8550aa4317093a9498ec51c6911f8319c51fe9f
Irina Tishchenko <irin.tishchenko@gmail.com>
2020-09-22 21:42:45 -0500

    login page button fix

--- src/pages/Login/Login.ts
+++ src/pages/Login/Login.ts
@@ -53,9 +53,7 @@ export class LoginPage extends Block {
             }
         }
 
-        document.querySelector('.button-submit').addEventListener('click', logIn);
-
-        //console.log("componentReady, this.formValidation = ", this.formValidation);
+        this.getContent().querySelector('.button-submit').addEventListener('click', logIn);
     }
 
     render() {
@@ -68,81 +66,3 @@ export class LoginPage extends Block {
         });
     }
 }
-
-// usernameInput {{chat-input-text 'login' 'Username' login}}
-// passwordInput {{chat-input-password 'password' 'Password' password}}
-// loginButton {{chat-button 'Log In' 'primary'}}
-// signupLink {{chat-link 'Need an account? Sign Up' "./signup.html"}}
-
-
-
-
-
-
-
-
-
-
-
-
-// import { loginTemplate } from './login.tmpl.js';
-// // import './helpers/helpers.js';
-// import { FormValidation } from '../../utils/formValidation.js';
-// import { handlebars } from '../../utils/Handlebars.js';
-// import { Block } from '../../utils/Block.js';
-
-// export class Login extends Block {
-//     constructor() {
-//         let props = {
-//             login: "",
-//             password: ""
-//         };
-
-//         super("div", props);
-//     }
-
-//     componentDidMount() {
-//         setTimeout(() => {
-//             this.setProps({
-//                 name: "Login 3",
-//                 password: "HAAAAA",
-//             });
-//         }, 5000);
-//     }
-
-//     render() {
-
-
-
-//         return compile(template, {
-//             userName: this.props.name,
-//             button: this.props.button.render()
-//         });
-//     }
-// }
-
-// const context = {
-//     login: "",
-//     password: ""
-// };
-
-// const template = handlebars().compile(loginTemplate);
-// const root = document.querySelector(".root");
-
-// root.innerHTML = template(context);
-
-// const formValidation = new FormValidation("form.login-dialog");
-
-// const logIn = () => {
-//     if (!formValidation.checkFormValidity()) {
-//         console.log("Form is invalid");
-//         formValidation.showErrors();
-//     } else {
-//         console.log('Logging in, data: ', JSON.stringify(formValidation.values));
-//     }
-// }
-
-// document.querySelector('.button-submit').addEventListener('click', logIn);
-
-// formValidation.setValidation("login");
-// formValidation.setValidation("password");

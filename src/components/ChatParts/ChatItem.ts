1e223cbfe19e249d435353aa20e7921ce7bbbe92
Irina Tishchenko <irin.tishchenko@gmail.com>
2020-09-24 11:10:27 -0500

    chat selectioon update

--- src/components/ChatParts/ChatItem.ts
+++ src/components/ChatParts/ChatItem.ts
@@ -4,12 +4,13 @@ import { template } from './ChatItem.tmpl.js';
 import { ChatInfo } from "../../business/ChatInfo.js";
 
 export class ChatItem extends Block {
-    constructor(chatInfo: ChatInfo, selected: boolean) {
+    constructor(chatInfo: ChatInfo) {
         super("li", {
+            user_id: chatInfo.user_id,
             avatarURL: chatInfo.avatar,
             display_name: chatInfo.display_name,
             last_message: chatInfo.last_message.json()
-        }, { classes: ["chatlist-item", `${selected ? "selected" : ""}`] });
+        }, { classes: ["chatlist-item"] });
     }
 
     componentRendered(): void {
--- src/pages/Chats/ChatMain.ts
+++ src/pages/Chats/ChatMain.ts
@@ -1,6 +1,4 @@
 import { Block } from "../../utils/Block.js";
-import { handlebars, handlebarsSafeString } from "../../utils/Handlebars.js";
-import { router } from "../../utils/Utils.js";
 import { template } from './ChatMain.tmpl.js';
 import { getAPIServer } from '../../server/Server.js';
 import { ChatItem } from "../../components/ChatParts/ChatItem.js";
@@ -10,7 +8,6 @@ export class ChatMainPage extends Block {
 
     constructor() {
         super("div", {
-            chatItems: "",
             loading: true
         }, { classes: ["chats-wrapper"] });
     }
@@ -19,46 +16,37 @@ export class ChatMainPage extends Block {
         this.drawChatList();
     }
 
+    chatItems: ChatItem[] = [];
+
+    chatSelected = (id: string) => {
+        this.chatItems.forEach((item) => {
+            if (item.props.user_id === id) {
+                item.select();
+            } else {
+                item.unselect();
+            }
+        });
+    }
+
     drawChatList(): void {
         let chatListLoader = this.getContent().querySelector(".chatlist-loader") as HTMLElement;
 
         getAPIServer().chats().then((data) => {
             chatListLoader.style.display = "none";
 
-            let chatItems: ChatItem[] = [];
-
-            const selectChatItem = (chatItem: ChatItem) => {
-                console.log("selected chatItem name = ", chatItem.props.display_name);
-                // chatItem.setProps({
-                //     display_name: chatItem.props.display_name + "1",
-                // });
-
-                chatItems.forEach((item) => {
-                    if (item == chatItem) {
-                        item.select();
-                    } else {
-                        item.unselect();
-                    }
-                });
-            }
-
             data.forEach((chatInfo) => {
-                let chatItemBlock = new ChatItem(chatInfo, false);
+                let chatItemBlock = new ChatItem(chatInfo);
                 chatItemBlock.componentRendered = () => {
-                    document.getElementById(chatItemBlock.id()).addEventListener('click', () => { selectChatItem(chatItemBlock); });
+                    document.getElementById(chatItemBlock.id()).addEventListener('click', () => { this.chatSelected(chatInfo.user_id); });
                 };
 
-                chatItems.push(chatItemBlock);
+                this.chatItems.push(chatItemBlock);
                 render(".chatlist-root", chatItemBlock);
             });
         });
     }
 
     render() {
-        const tmpl = handlebars().compile(template);
-        return tmpl({
-            loading: this.props.loading,
-            chatItems: this.props.chatItems
-        });
+        return template;
     }
 }
